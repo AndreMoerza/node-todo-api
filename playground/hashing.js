100a57d2d66cf3d2b7025fb6b7101112fd93066c
Andre Pratama <Andre_P772@mitrais.com>
2019-02-07 10:55:53 +0700

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,24 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abac');
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedpassword = '$2a$10$r.kj3pgS8a04lit7.oMoNe.qtSK9BCHHeAuT8nsdIbDhEL/aqI5hC';
+bcrypt.compare(password, hashedpassword, (err, res) => {
+    console.log(res);
+});
+/*var token = jwt.sign(data, '123abac');
 console.log(token);
 
 var decoded = jwt.verify(token, '123abac');
-console.log(decoded);
+console.log(decoded);*/
 /*var message = 'I am user number 3';
 var hash = SHA256(message).toString();
 
